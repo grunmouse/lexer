fac56a742c0bb2219adbef4eae2288f946124048
Dmitry Rakov <rakov@mirapolis.ru>
2022-01-02 20:36:41 +0300

    init

--- test/integer-area-test.js
+++ test/integer-area-test.js
@@ -1,6 +1,13 @@
 const assert = require('assert');
 const IntegerArea = require('../integer-area.js');
 
+const {
+	prepForAll,
+	env
+} = require('@grunmouse/jsverify-env');
+
+const jsc = require('jsverify');
+
 describe('IntegerArea', ()=>{
 	describe('exist', ()=>{
 		it('has constructor', ()=>{
@@ -9,6 +16,16 @@ describe('IntegerArea', ()=>{
 		it('create instance', ()=>{
 			let inst = new IntegerArea(1,2);
 			assert(inst instanceof IntegerArea);
+		});
+	});
+	
+	describe('limits', ()=>{
+		let [min, max] = env.incarray(2, env.uint32).generator();
+		let over = max+1;
+		console.log(min, max);
+		jsc.property('inner', env.int(min, max), env, (value)=>{
+			let area = new IntegerArea(min, over);
+			return area.has(value);
 		});
 	});
 });
